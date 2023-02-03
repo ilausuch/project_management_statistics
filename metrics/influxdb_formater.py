99f2ec6962d6875461f4991938c9d83349149d9c
Ivan Lausuch <ilausuch@suse.com>
2023-02-03 13:40:51 +0100

    Add influx db formater

---
+++ metrics/influxdb_formater.py
@@ -0,0 +1,14 @@
+class MetricsInfluxdbFormater:
+    @staticmethod
+    def format_dict(measurement_name, values, date, **filter):
+        """
+        Format a dictionary with the format key:value
+        :param measurement_name: Messurement name for the influx db
+        :param values: the dictionary with the format <status_id:count>
+        :param date: the date of the measurement
+        :param filter: the filters used to obtain this messurement
+        :return: A formated string
+        """
+        filter_str = ','.join([f"{key}={value}" for key, value in filter.items()])
+        values_str = ','.join([f"{key}={value}" for key, value in values.items()])
+        return f"{measurement_name} {filter_str} {values_str} {date.strftime('%Y-%m-%dT%H:%M:%S.%fZ')}"
