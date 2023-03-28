9cb68cd61e4b08d0bb5d078afc10310c7c9e1431
ybonatakis <ybonatakis@suse.com>
2023-03-28 14:15:25 +0200

    Use subcommands instead of argument on dumper
    Refactor argparser to make separate logic into separate group of subcommands
    which each takes its own arguments. This is more clear representation of
    various implentations and easy to understand and extend without breaking
    individual components or conflicts between each other
    
    Signed-off-by: ybonatakis <ybonatakis@suse.com>

--- dumper.py
+++ dumper.py
@@ -1,17 +1,60 @@
-#!/usr/local/bin/python3
-import logging
+#!/usr/bin/env python3
 import argparse
+import logging
+import logging.config
+
 from redmine.redmine_dumper import RedmineDumper
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--source', help="Source from where ticket will be dumped. Possible values: (redmine|bugzilla). \
-            Default: redmine", default='redmine')
-    parser.add_argument('--database', help="SQLite file path", required=True)
-    parser.add_argument('--project', help="Project name")
-    parser.add_argument('-v', action='store_true', help='increase output verbosity')
+    parser.add_argument('-v',
+                        action='store_true',
+                        help='increase output verbosity')
+    subparsers = parser.add_subparsers(dest='source', required=True)
+    redmine_parser = subparsers.add_parser('redmine', help='redmine help')
+    redmine_parser.add_argument('--project', help="Project name")
+    redmine_parser.add_argument('--database',
+                                help="SQLite file path",
+                                required=True)
+
+    bugzilla_parser = subparsers.add_parser('bugzilla', help='bugzilla help')
+    bugzilla_parser.add_argument('-P', '--prod', metavar='string', type=str,
+                                 help='Bugzilla product value')
+    bugzilla_parser.add_argument('-s', '--status', metavar='string', type=str,
+                                 choices=['NEW',
+                                          'CONFIRMED',
+                                          'IN_PROGRESS',
+                                          'RESOLVED'],
+                                 help='What status should be the bugs')
+    bugzilla_parser.add_argument('-S', '--severity',
+                                 metavar='string', type=str,
+                                 choices=['Critical',
+                                          'Major',
+                                          'Normal',
+                                          'Minor',
+                                          'Enhancement'],
+                                 help='what severity should the query use')
+    bugzilla_parser.add_argument('-p', '--priority',
+                                 metavar='string', type=str,
+                                 choices=['P0 - Crit Sit',
+                                          'P1 - Urgent',
+                                          'P2 - High',
+                                          'P3 - Medium',
+                                          'P4 - Low',
+                                          'P5 - None'],
+                                 help='What priority should we query')
+    bugzilla_parser.add_argument('-n', '--range',
+                                 metavar='int', type=int,
+                                 default=max,
+                                 help='An integer value which will give the \
+                                 range of the query since current date')
+    bugzilla_parser.add_argument('-x', '--exec', nargs='*',
+                                 help='List of bsc_queries to run')
+    bugzilla_parser.add_argument('-l', action='store_true',
+                                 dest='listqueries', required=False,
+                                 help='List the metric queries \
+                                 which can be used')
     args = parser.parse_args()
 
     if args.v is True:
@@ -23,9 +66,11 @@ def main():
 
     if args.source == "redmine":
         if args.project is None:
-            raise TypeError("dumper.py: error: the following arguments are required: --project when source is redmine")
+            raise TypeError("dumper.py: error: the following arguments are \
+            required: --project when source is redmine")
 
-        logger.info("Dumping from %s (project: %s) to database %s", args.source, args.project, args.database)
+        logger.info("Dumping from %s (project: %s) to database %s",
+                    args.source, args.project, args.database)
         dumper = RedmineDumper(args.database, logging_level)
         dumper.dump_to_db(args.project)
     else:
