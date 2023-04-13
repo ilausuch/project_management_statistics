13c2d7a8b598226ef272667e6a2aefd270dec2b3
Ivan Lausuch <ilausuch@suse.com>
2023-04-13 09:48:40 +0200

    Fix sqlite_query
    In some cases where there are not enough information in IssueEvents
    the method status_snapshot cannot work properly
    Also it was using __dict__ to obtain a dictionary from the models
    but changes on this dictionary do changes on the DB

--- tests/test_sqlite_query.py
+++ tests/test_sqlite_query.py
@@ -7,74 +7,87 @@ def test_status_snapshot():
     query = SQLiteQuery(":memory:")
     session = query.session
 
+    # Dates
     date_on_new = datetime(2023, 1, 1)
     date_move_on_in_progress = datetime(2023, 1, 15)
     date_move_on_resolved = datetime(2023, 2, 1)
+    date_move_on_closed = datetime(2023, 2, 15)
 
-    session.add(
-        Issue(
-            issue_id=1,
-            project=1,
-            status="RESOLVED",
-            created_on=date_on_new,
-            closed_on=date_move_on_resolved,
-        )
-    )
-    session.add(
-        IssueEvent(
-            issue_id=1,
-            type="attr",
-            field="status",
-            created_on=date_on_new,
-            new_value="NEW"
-        )
-    )
-    session.add(
-        IssueEvent(
-            issue_id=1,
-            type="attr",
-            field="status",
-            created_on=date_move_on_in_progress,
-            old_value="WORKABLE",
-            new_value="IN_PROGRESS"
-        )
-    )
-    session.add(
-        IssueEvent(
-            issue_id=1,
-            type="attr",
-            field="status",
-            created_on=date_move_on_resolved,
-            old_value="IN_PROGRESS",
-            new_value="RESOLVED"
-        )
-    )
+    # Issues
+    issue1 = Issue(issue_id=1, project=1, status="Closed", created_on=date_on_new, closed_on=date_move_on_closed)
+    issue2 = Issue(issue_id=2, project=1, status="Resolved", created_on=date_on_new, closed_on=date_move_on_resolved)
+    issue3 = Issue(issue_id=3, project=1, status="Resolved", created_on=date_on_new, closed_on=date_move_on_resolved)
+    issue4 = Issue(issue_id=4, project=1, status="In_Progress", created_on=date_on_new, closed_on=None)
+
+    session.add_all([issue1, issue2, issue3, issue4])
+
+    # Issue events for issue 1
+    session.add_all([
+        IssueEvent(issue_id=1, type="attr", field="status", created_on=date_on_new, new_value="New"),
+        IssueEvent(issue_id=1, type="attr", field="status", created_on=date_move_on_in_progress,
+                   old_value="New", new_value="In_Progress"),
+        IssueEvent(issue_id=1, type="attr", field="status", created_on=date_move_on_resolved,
+                   old_value="In_Progress", new_value="Resolved"),
+        IssueEvent(issue_id=1, type="attr", field="status", created_on=date_move_on_closed,
+                   old_value="Resolved", new_value="Closed"),
+    ])
+
+    # Issue events for issue 2
+    session.add_all([
+        IssueEvent(issue_id=2, type="attr", field="status", created_on=date_on_new, new_value="New"),
+        IssueEvent(issue_id=2, type="attr", field="status", created_on=date_move_on_in_progress,
+                   old_value="New", new_value="In_Progress"),
+        IssueEvent(issue_id=2, type="attr", field="status", created_on=date_move_on_resolved,
+                   old_value="In_Progress", new_value="Resolved"),
+    ])
+
+    # Issue events for issue 4
+    session.add_all([
+        IssueEvent(issue_id=4, type="attr", field="status", created_on=date_move_on_in_progress,
+                   old_value="New", new_value="In_Progress")
+    ])
+
     session.commit()
 
-    # Get the current state for the issue (should be one)
-    result = query.issues(project=1)
-    assert len(result) == 1
-    assert result[0]["status"] == str("RESOLVED")
-
+    # Test case 1: Snapshot for an issue without an initial IssueEvent
     result = query.status_snapshot(date=date_on_new, project=1)
-    assert len(result) == 1
-    assert result[0]["status"] == str("NEW")
+    assert len(result) == 4
+    assert result[0]["status"] == "New"
+    assert result[1]["status"] == "New"
+    assert result[2]["status"] == "New"
+    assert result[3]["status"] == "New"
 
+    # Test case 2: Snapshot on the date of issue2 being resolved
     result = query.status_snapshot(date=date_move_on_in_progress, project=1)
-    assert len(result) == 1
-    assert result[0]["status"] == str("IN_PROGRESS")
+    assert len(result) == 4
+    assert result[0]["status"] == "In_Progress"
+    assert result[1]["status"] == "In_Progress"
+    assert result[2]["status"] == "New"
+    assert result[3]["status"] == "In_Progress"
 
+    # Test case 3: Snapshot on the date of issue2 being resolved
     result = query.status_snapshot(date=date_move_on_resolved, project=1)
-    assert len(result) == 1
-    assert result[0]["status"] == str("RESOLVED")
+    assert len(result) == 4
+    assert result[0]["status"] == "Resolved"
+    assert result[1]["status"] == "Resolved"
+    assert result[2]["status"] == "Resolved"
+    assert result[3]["status"] == "In_Progress"
 
-    result = query.status_snapshot(date=datetime(2024, 1, 1), project=1)
-    assert len(result) == 1
-    assert result[0]["status"] == str("RESOLVED")
+    # Test case 4: Snapshot on the date of issue1 being closed
+    result = query.status_snapshot(date=date_move_on_closed, project=1)
+    assert len(result) == 4
+    assert result[0]["status"] == "Closed"
+    assert result[1]["status"] == "Resolved"
+    assert result[2]["status"] == "Resolved"
+    assert result[3]["status"] == "In_Progress"
 
-    # The date before the issue is created
-    result = query.status_snapshot(date=datetime(2022, 1, 1), project=1)
-    assert len(result) == 0
+    # Test case 5: Snapshot on a date after all the events
+    result = query.status_snapshot(date=datetime(2023, 3, 1), project=1)
+    assert len(result) == 4
+    assert result[0]["status"] == "Closed"
+    assert result[1]["status"] == "Resolved"
+    assert result[2]["status"] == "Resolved"
+    assert result[3]["status"] == "In_Progress"
 
     session.close()
 
@@ -95,49 +108,48 @@ def test_issues_active_in_period():
     session.add(Issue(
         issue_id=1,
         project=1,
-        status="RESOLVED",
+        status="Resolved",
         created_on=date_before_period_in_1,
         closed_on=date_before_period_in_2
     ))
     session.add(Issue(
         issue_id=2,
         project=1,
-        status="RESOLVED",
+        status="Resolved",
         created_on=date_before_period_in_1,
         closed_on=date_period_1
     ))
     session.add(Issue(
         issue_id=3,
         project=1,
-        status="RESOLVED",
+        status="Resolved",
         created_on=date_before_period_in_1,
         closed_on=date_after_period_out_1
     ))
     session.add(Issue(
         issue_id=4,
         project=1,
-        status="RESOLVED",
+        status="Resolved",
         created_on=date_period_1,
         closed_on=date_period_2
     ))
     session.add(Issue(
         issue_id=5,
         project=1,
-        status="RESOLVED",
+        status="Resolved",
         created_on=date_period_2,
         closed_on=date_after_period_out_1
     ))
     session.add(Issue(
         issue_id=6,
         project=1,
-        status="RESOLVED",
+        status="Resolved",
         created_on=date_after_period_out_1,
         closed_on=date_after_period_out_2
     ))
     session.commit()
 
     result = query.issues_active_in_period(date_in=date_period_in, date_out=date_period_out, project=1)
-    print(result)
     assert len(result) == 4
 
     session.close()
